4047cde5752dae97e90d10a0db4c0209211472c5
Alexander Zotter <91153474+taktischer@users.noreply.github.com>
2023-09-29 11:16:41 +0200

    created IndexView; added AppointmentModel;

--- web_app/models.py
+++ web_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User as django_User
 
-# Create your models here.
+
+class Appointment(models.Model):
+    user = models.ForeignKey(django_User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=40, blank=False, null=False)
+    description = models.CharField(max_length=500, blank=False, null=False)
+    start_time = models.DateTimeField(blank=False, null=False)
+    end_time = models.DateTimeField(blank=True, null=True)
