db517f22473c2360d9ba811b7a8a696d7c2a0065
Alexander Zotter <91153474+taktischer@users.noreply.github.com>
2023-11-15 10:37:43 +0100

    modified AppointmentCreateView
    - added UserPassesTestMixin
    - creates Appointment on submit

--- web_app/views.py
+++ web_app/views.py
@@ -1,16 +1,33 @@
+from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic import TemplateView, FormView
 
 from web_app.forms import AppointmentCreateForm
+from web_app.models import Appointment
 
 
 class IndexView(TemplateView):
     template_name = "index.html"
 
 
-class AppointmentCreateView(FormView):
+class AppointmentCreateView(UserPassesTestMixin, FormView):
     template_name = "appointment_create.html"
     form_class = AppointmentCreateForm
+    success_url = "/"
+
+    def test_func(self):
+        return self.request.user.is_authenticated
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+    def form_valid(self, form):
+        post_data = self.request.POST
+        print(post_data)
+        print(self.request.user)
+        Appointment.objects.create(user=self.request.user,
+                                   title=post_data['title'],
+                                   description=post_data['description'],
+                                   start_time=post_data['start_time'],
+                                   end_time=post_data['end_time'])
+        return super().form_valid(form)
