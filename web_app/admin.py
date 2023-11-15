bb86ab79d78036b8a831cc61cf7c484c773d69aa
benjibalci <benjamin.balcinovic@bulme.at>
2023-11-15 09:30:49 +0100

    deleted everything

--- web_app/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-from web_app.models import Appointment
-# Register your models here.
-admin.site.register(Appointment)
--- web_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from web_app import views
-
-urlpatterns = [
-    path("", views.IndexView.as_view(), name="index"),
-    path("balcinovic/", views.BalcinovicView.as_view(), name="balcinovic")
-]
