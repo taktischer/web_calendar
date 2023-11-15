087bb56ed86633c9239db150b6869e2f74c446cf
benjibalci <benjamin.balcinovic@bulme.at>
2023-11-15 10:37:34 +0100

    added static folder

--- web_calendar/settings.py
+++ web_calendar/settings.py
@@ -119,6 +119,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    "static",
+    "images",
+    "scripts"
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
